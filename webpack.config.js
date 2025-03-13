855cb9cd8f84a6c087c7410a7a3a4ed18b1d6540
Andrey Isaev <andrey.isaev.54.ru@gmail.com>
2025-03-13 03:37:00 +0300

    feat: header style

--- webpack.config.js
+++ webpack.config.js
@@ -6,15 +6,6 @@ const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
   entry: './src/index.ts',
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
   resolve: {
     extensions: ['.ts', '.js', '.tsx'],
   },
@@ -24,27 +15,45 @@ module.exports = {
   },
   mode: 'development',
   devServer: {
-    static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
+    static: {
+      directory: path.join(__dirname, 'dist'), // Папка для статических файлов
+    },
     compress: true, // это ускорит загрузку в режиме разработки
     port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
 
     open: true // сайт будет открываться сам при запуске npm run dev
   },
-  module: { rules: [{ test: /\.tsx?$/, exclude: ['/node_modules/', /\.(d.ts)$/], use: { loader: 'babel-loader', options: { presets: ['@babel/preset-env', '@babel/preset-typescript'] } } }, 
-  {
-    // регулярное выражение, которое ищет все файлы с такими расширениями
-    test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-    type: 'asset/resource'
-  },
-  {
-    test: /\.scss$/,
-    use: [
-      MiniCssExtractPlugin.loader, // Извлекает CSS в отдельные файлы
-      'css-loader', // Переводит CSS в CommonJS
-      'sass-loader', // Компилирует SCSS в CSS
+   module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: ['/node_modules/', /\.(d.ts)$/],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-typescript'],
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader, // Извлекает CSS в отдельные файлы
+          'css-loader', // Переводит CSS в CommonJS
+          'sass-loader', // Компилирует SCSS в CSS
+        ],
+      },
     ],
   },
-  ] },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html' // путь к файлу index.html
@@ -55,6 +64,7 @@ module.exports = {
     }),
   ],
   optimization: {
+    minimize: true,
     minimizer: [
       new CssMinimizerPlugin(), // Минифицирует CSS
     ],
